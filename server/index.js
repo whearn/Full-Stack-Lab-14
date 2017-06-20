07a6a3dab27ddff3fcb5f4e5a70d118c3f7b5227
whearn <wchearn00@gmail.com>
2017-06-20 10:33:38 -0400

    Finished Lab With Express and Promise Versions

--- server/index.js
+++ server/index.js
@@ -1,53 +1,102 @@
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var app = express();
+//No promises
+// var path = require('path');
+// var fs = require('fs');
+// var express = require('express');
+// var bodyParser = require('body-parser');
 
+// //find the client/frontend folder
+// var clientPath = path.join(__dirname, '../client');
+// var dataPath = path.join(__dirname, 'data.json');
+
+// var app = express();
+// //serve files from the client/frontend folder
+// app.use(express.static(clientPath));
+// app.use(bodyParser.json());
+
+// app.route('/api/chirps')
+//     .get(function(req, res) {
+//         res.sendFile(dataPath);
+//     }).post(function(req, res) {
+//         var newChirp = req.body;
+//         fs.readFile(dataPath, 'utf8', function(err, fileContents) {
+//             if (err) {
+//                 console.log(err);
+//                 res.sendStatus(500);
+//             } else {
+//                 var chirps = JSON.parse(fileContents);
+//                 chirps.push(newChirp);
+//                 fs.writeFile(dataPath, JSON.stringify(chirps), function(err) {
+//                     if (err) {
+//                         console.log(err);
+//                         res.sendStatus(500);
+//                     } else {
+//                         res.sendStatus(201);
+//                     }
+//                 });
+//             }
+//         });
+//     });
+
+// app.listen(3000);
+
+
+
+//In Class Solution With Promises
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//find the client/frontend folder
 var clientPath = path.join(__dirname, '../client');
 var dataPath = path.join(__dirname, 'data.json');
 
-var fileServer = express.static(clientPath);
-app.use(fileServer);
+var app = express();
+//serve files from the client/frontend folder
+app.use(express.static(clientPath));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-    res.send('');
-});
-
-app.get('/api/chirps', function(req, res) {
-    res.send('');
-});
-
-app.post('/api/chirps', function(req, res) {
-    fs.readFile(dataPath, 'utf8', function(err, fileContents) {
-        if (err) {
-            console.log(err);
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-        } else {
+app.route('/api/chirps')
+    .get(function(req, res) {
+        res.sendFile(dataPath);
+    }).post(function(req, res) {
+        var newChirp = req.body;
+        accessFile(dataPath, 'utf8')
+        .then(function(fileContents) {
             var chirps = JSON.parse(fileContents);
-
-            var incomingData = '';
-            req.on('data', function(chunk) {
-                incomingData += chunk;
-            });
-            req.on('end', function() {
-                var newChirp = JSON.parse(incomingData);
-                chirps.push(newChirp);
-
-                var chirpsJSONData = JSON.stringify(chirps);
-                fs.writeFile(dataPath, chirpsJSONData, function(err) {
-                    if (err) {
-                        console.log(err);
-                        res.writeHead(500, { 'Content-Type': 'text/plain' });
-                        res.end('Internal Server Error');
-                    } else {
-                        res.writeHead(201);
-                        res.end();
-                    }
-                });
-            });
-        }
+            chirps.push(newChirp);
+            return writeFile(dataPath, JSON.stringify(chirps));
+        }).then(function() {
+            res.sendStatus(201);
+        }).catch(function(err) {
+            console.log(err);
+            res.sendStatus(500);
+        });
     });
-});
 
 app.listen(3000);
+
+//where can I find the file, and what is its encoding
+function accessFile(filePath, encoding) {
+    return new Promise(function(resolve, reject) {
+        fs.readFile(filePath, encoding, function(err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+function writeFile(filePath, data) {
+    return new Promise(function(resolve, reject) {
+        fs.writeFile(filePath, data, function(err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
